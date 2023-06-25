8d72131ad1c4961c4b048b4eda392b512dea3188
VaalaCat <me@vaala.cat>
2023-06-25 15:43:25 +0800

    fix: ping err

--- utils/request/ping.go
+++ utils/request/ping.go
@@ -10,7 +10,7 @@ import (
 func Ping(endpoint, host string) (t int, err error) {
 	t1 := time.Now()
 	resp, err := req.C().R().SetHeader("Host", host).Get(endpoint)
-	if err != nil && resp.StatusCode != http.StatusOK {
+	if err != nil || resp.StatusCode != http.StatusOK {
 		return 9999, err
 	}
 	t2 := time.Now()
